8efee3086744f4efb69839fa2b7e5add85f60437
AlexeyTolmachov <taar12sh@gmail.com>
2024-03-10 12:53:34 +0200

    add router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const auth_router = require("./routes/auth_router")
-
+const product_router = require("./routes/product_router");
 app.use(express.json());
 app.use("/auth", auth_router);
+app.use("/product", product_router);
 
 
 module.exports = app;
