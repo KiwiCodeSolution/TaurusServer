b318910a891bb22b4f1b913c94d582d9bb288137
AlexeyTolmachov <taar12sh@gmail.com>
2024-04-01 22:56:13 +0300

    add routers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const auth_router = require("./routes/auth_router")
 const product_router = require("./routes/product_router");
+const order_router = require('./routes/order_router')
 app.use(express.json());
 app.use("/auth", auth_router);
 app.use("/product", product_router);
+app.use("/order", order_router);
+// app.use("/reservation", reservation_router);
 
 
 module.exports = app;
---
+++ src/routes/reservation_router.js
@@ -0,0 +1,9 @@
+// const express = require("express");
+// const router = express.Router();
+// const controller = require("../controllers/product_controller");
+// const ctrlWrapper = require("../middlewares/ctrlWrapper");
+
+// // POST /products/consent
+// router.post("/consent", ctrlWrapper(controller.process_personal_data_consent));
+
+// module.exports = router;
