6582628944d0f16c8fdc13e6716743538bb948f8
AlexeyTolmachov <taar12sh@gmail.com>
2024-04-02 22:10:39 +0300

    create reservation, feedback, delivery router

---
+++ src/routes/feedback_router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const feedbackController = require('../controllers/feedback_controller');
+
+router.post('/', feedbackController.createFeedback);
+router.get('/', feedbackController.getAllFeedbacks);
+router.get('/:id', feedbackController.getFeedbackById);
+router.delete('/:id', feedbackController.deleteFeedbackById);
+
+module.exports = router;
--- src/routes/reservation_router.js
+++ src/routes/reservation_router.js
@@ -1,9 +1,11 @@
-// const express = require("express");
-// const router = express.Router();
-// const controller = require("../controllers/product_controller");
-// const ctrlWrapper = require("../middlewares/ctrlWrapper");
+const express = require('express');
+const router = express.Router();
+const reservationController = require('../controllers/reservation_controller');
 
-// // POST /products/consent
-// router.post("/consent", ctrlWrapper(controller.process_personal_data_consent));
+router.get('/', reservationController.getAllReservations);
+router.post('/', reservationController.createReservation);
+router.get('/:id', reservationController.getReservationById);
+router.put('/:id', reservationController.updateReservation);
+router.delete('/:id', reservationController.deleteReservation);
 
-// module.exports = router;
+module.exports = router;
