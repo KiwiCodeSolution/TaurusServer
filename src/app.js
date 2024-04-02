0de785abe753420ea26285d87715a33d43a126b5
AlexeyTolmachov <taar12sh@gmail.com>
2024-04-02 22:10:54 +0300

    update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,17 @@ const app = express();
 const auth_router = require("./routes/auth_router")
 const product_router = require("./routes/product_router");
 const order_router = require('./routes/order_router')
+const delivey_router = require('./routes/delivery_router')
+const feedback_router = require('./routes/feedback_router')
+const reservations_router = require('./routes/reservation_router')
 app.use(express.json());
 app.use("/auth", auth_router);
 app.use("/product", product_router);
 app.use("/order", order_router);
-// app.use("/reservation", reservation_router);
+app.use("/delivery", delivey_router);
+app.use("/feedback", feedback_router);
+app.use("/reservations", reservations_router);
+
 
 
 module.exports = app;
