b55d61b1d502637aedda225eca768c8b5f764c5e
AlexeyTolmachov <taar12sh@gmail.com>
2024-05-07 13:13:49 +0300

    update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const cors = require('cors');
+const multer = require("multer");
 
 const auth_router = require("./routes/auth_router")
 const product_router = require("./routes/product_router");
@@ -8,8 +11,11 @@ const delivey_router = require('./routes/delivery_router')
 const feedback_router = require('./routes/feedback_router')
 const reservations_router = require('./routes/reservation_router')
 const promotions_router = require('./routes/promotions_router')
-
+const upload_router = require("./routes/upload_router");
+app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", upload_router);
 app.use("/auth", auth_router);
 app.use("/product", product_router);
 app.use("/order", order_router);
@@ -19,9 +25,15 @@ app.use("/reservations", reservations_router);
 app.use("/sale", reservations_router);
 app.use("/promotions", promotions_router);
 
-
 app.use((req, res) => {
 	res.status(404).json({ message: "Route not found" });
 });
+app.use(async (err, req, res, next) => {
+	if (err instanceof multer.MulterError) {
+		err.status = 400;
+	}
 
+	const { status = 500, message = "Server error" } = err;
+	res.status(status).json({ message });
+});
 module.exports = app;
