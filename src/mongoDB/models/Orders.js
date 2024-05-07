fc32b0d8abdd3bef359e9f82bda3b3060442e374
AlexeyTolmachov <taar12sh@gmail.com>
2024-05-07 13:13:26 +0300

    update model orders

--- src/mongoDB/models/Orders.js
+++ src/mongoDB/models/Orders.js
@@ -1,4 +1,36 @@
+// const { Schema, model } = require("mongoose");
+
+// const OrderSchema = new Schema({
+// 	order_number: { type: Number, required: true, unique: true },
+// 	order_date: {
+// 		type: Date,
+// 		required: true,
+// 		default: () => {
+// 			const now = new Date();
+// 			now.setHours(now.getHours() + 3); // Смещение для Киева (UTC+3)
+// 			return now;
+// 		}
+// 	},
+// 	delivery_type: { type: String, enum: ["У ресторані", "Доставка"], required: true },
+// 	products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true }],
+// 	name: { type: String, required: true },
+// 	email: { type: String, required: true },
+// 	phone: { type: String, required: true },
+// 	date: { type: Date, required: true },
+// 	time: { type: String, required: true },
+// 	message: { type: String },
+// 	consent: { type: Boolean, required: true },
+// 	total_cost: { type: Number, required: true }, // Добавлено поле общей стоимости
+// 	address: { type: String, required: false } // Добавлено поле адреса
+// });
+
+// module.exports = model("Order", OrderSchema);
 const { Schema, model } = require("mongoose");
+
+const OrderProductSchema = new Schema({
+	product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+	quantity: { type: Number, required: true }
+});
 
 const OrderSchema = new Schema({
 	order_number: { type: Number, required: true, unique: true },
@@ -12,7 +44,7 @@ const OrderSchema = new Schema({
 		}
 	},
 	delivery_type: { type: String, enum: ["У ресторані", "Доставка"], required: true },
-	products: [{ type: Schema.Types.ObjectId, ref: 'Product', required: true }],
+	products: [OrderProductSchema], // Массив объектов заказанных продуктов
 	name: { type: String, required: true },
 	email: { type: String, required: true },
 	phone: { type: String, required: true },
