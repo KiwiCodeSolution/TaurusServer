dc945a2a4ba625649015aabf1b13105f651ee2d6
AlexeyTolmachov <taar12sh@gmail.com>
2024-05-16 10:53:58 +0300

    update models

--- src/mongoDB/models/Feedback.js
+++ src/mongoDB/models/Feedback.js
@@ -6,7 +6,9 @@ const FeedbackSchema = new Schema(
 		email: { type: String, required: true },
 		phone: { type: String },
 		message: { type: String },
-		consent: { type: Boolean, required: true }
+		consent: { type: Boolean, required: true },
+		status: { type: String, enum: ["new", "in_progress", "completed"], default: "new" }, // Добавляем статусы: новый, в работе, выполнен
+		archive: { type: Boolean, default: false } // Поле для архивирования
 	},
 	{ timestamps: true }
 );
