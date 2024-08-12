cc9d402d2fc416056218af2a1fd07c924ea4121b
AlexeyTolmachov <taar12sh@gmail.com>
2024-08-12 22:24:01 +0300

    activated register

--- src/routes/auth_router.js
+++ src/routes/auth_router.js
@@ -12,5 +12,5 @@ router.get("/getCurrent", auth, ctrlWrapper(auth_controller.getCurrent));
 router.get("/all", auth, ctrlWrapper(auth_controller.getAllUsers)); 
 //router.put('/:id', auth, ctrlWrapper(auth_controller.toggleUserActivation)); 
 //router.post('/reset_password', auth, superadmin, ctrlWrapper(auth_controller.resetPassword));
-//router.post("/register", ctrlWrapper(auth_controller.register));
+router.post("/register", ctrlWrapper(auth_controller.register));
 module.exports = router;
