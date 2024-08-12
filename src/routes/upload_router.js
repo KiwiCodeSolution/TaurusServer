ba423a098e00f0b7ef19aabec6191c9f66bb812b
AlexeyTolmachov <taar12sh@gmail.com>
2024-08-12 22:26:15 +0300

    add auth in upload img

--- src/routes/upload_router.js
+++ src/routes/upload_router.js
@@ -2,9 +2,10 @@ const Router = require("express");
 const router = new Router();
 const img_controller = require("../controllers/img_controller");
 const ctrlWrapper = require("../middlewares/ctrlWrapper");
+const auth = require("../middlewares/auth_middleware")
 
 // поставить auth_middleware после тестирования
-router.post("/", img_controller.upload.single("image"), ctrlWrapper(img_controller.upload_img));
+router.post("/",auth, img_controller.upload.single("image"), ctrlWrapper(img_controller.upload_img));
 
 router.get("/:img", ctrlWrapper(img_controller.get_img));
 
